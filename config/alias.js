238ae2499ae1a2b57c3888dd2add5dd025b41492
Rtdxz <1712045716@qq.com>
2021-06-20 23:36:44 +0800

    chore: 完成初始化

---
+++ config/alias.js
@@ -0,0 +1,11 @@
+module.exports = {
+  COMMON: 'common',
+  ASSETS: 'common/assets',
+  'XHR-AXIOS': 'common/xhr-axios',
+  '@src': 'src',
+  '@assets': 'src/assets',
+  '@views': 'src/views',
+  '@components': 'src/components',
+  '@services': 'src/services',
+  '@constant': 'src/constant',
+};
