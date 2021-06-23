946cef4daf5a2663b253ab487be8af0507a6e55a
Rtdxz <1712045716@qq.com>
2021-06-23 20:21:48 +0800

    feat: 添加Svg组件

---
+++ src/components/svg/SvgIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="currentComponent" class="svg-icon" />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import SvgIconCloud from './SvgIconCloud.vue';
+
+@Component({
+  components: {
+    SvgIconCloud,
+  },
+})
+export default class SvgIcon extends Vue {
+  @Prop({ default: '' })
+  type!: string;
+
+  get currentComponent() {
+    return `svg-icon-${this.type}`;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+// @import './index.scss';
+.svg-icon {
+  fill: currentColor;
+}
+</style>
