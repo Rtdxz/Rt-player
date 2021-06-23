d6a029796e248ced2987a5991f19bf4ad7abda48
Rtdxz <1712045716@qq.com>
2021-06-23 11:07:38 +0800

    chore: 部署热更新

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,10 @@ module.exports = {
       config.resolve.alias.set(val, path.resolve(rootPath, alias[val]));
     });
   },
+  devServer : {
+    hot:true,//自动保存
+    open : true,//自动启动
+    port : 8080,//默认端口号
+    host : "0.0.0.0"
+}
 };
